85a5c4fffa54fe4cfba16ceeee5fa38cd3369957
Aaarke <roshan.k@toppr.com>
2021-02-22 00:48:07 +0530

    test cases added for bankcontroller

--- src/main/kotlin/com/example/roshi/controller/BankController.kt
+++ src/main/kotlin/com/example/roshi/controller/BankController.kt
@@ -1,9 +1,16 @@
 package com.example.roshi.controller
 
+import com.example.roshi.model.Bank
+import com.example.roshi.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class BankController {
+@RequestMapping("/api/banks")
+class BankController(private val service: BankService) {
+    @GetMapping
+    fun getBanks():Collection<Bank> = service.getBanks()
 
 }
